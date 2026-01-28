abb39c8510b8e8d84edb075a94346204fbdc4625
mtqiaohub <mtqiao@wustl.edu>
2026-01-28 14:47:34 -0600

    finished working version of local csv passed to robot through http

--- play_run.py
+++ play_run.py
@@ -1,25 +1,58 @@
 import requests
-import json
-
+import tempfile
+import os
 
 ROBOT_IP = "10.70.18.217"
-RUN_ID = "4939d899-1ec0-4aff-bffc-f0d1a96ba1f8"
 
 HEADERS = {"opentrons-version": "3"}
 
-#4939d899-1ec0-4aff-bffc-f0d1a96ba1f8 ['test_http_api.py', 'eppendorf_rack_lid.json', 'custom_tilting_reservoir_7500_tilted.json', 'custom_tilting_reservoir_lower.json', 'custom_tilting_reservoir_7500.json', 'biotdot96wellpcrplate_96_wellplate_200ul.json'] 2026-01-27T22:16:30.216298Z
+#a57c644a-ce8a-4e99-ae23-98211362e7d6
 #protocol id for testing http_api
 
-protocol_id = "4939d899-1ec0-4aff-bffc-f0d1a96ba1f8"
+protocol_id = "6f2eb7d3-3dd0-43de-95a8-7dea0846bf6e"
 
-# create run
+
+
+csv_contents = """well,has_tip
+A1,1
+A2,0
+A3,1
+B1,0
+"""
+
+with tempfile.NamedTemporaryFile(delete=False, suffix=".csv", mode="w") as f:
+    f.write(csv_contents)
+    csv_path = f.name
+
+with open(csv_path, "rb") as f:
+    r = requests.post(
+        f"http://{ROBOT_IP}:31950/dataFiles",
+        headers=HEADERS,
+        files={"file": f},
+    )
+r.raise_for_status()
+
+
+data_file_id = r.json()["data"]["id"]
+print("Uploaded file_id:", data_file_id)
+
+
 r = requests.post(
     f"http://{ROBOT_IP}:31950/runs",
     headers=HEADERS,
-    json={"data": {"protocolId": protocol_id}},
+    json={
+        "data": {
+            "protocolId": protocol_id,
+            "runTimeParameterFiles": {
+                "tiprack_state_csv": data_file_id
+            }
+        }
+    },
 )
 r.raise_for_status()
+
 run_id = r.json()["data"]["id"]
+print("Created run_id:", run_id)
 
 # play run
 r = requests.post(
@@ -29,3 +62,6 @@ r = requests.post(
 )
 r.raise_for_status()
 
+print("Run started")
+
+os.unlink(csv_path)
